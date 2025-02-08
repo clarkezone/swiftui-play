8cab3cb3165263881ea9076084e927b8574c81f8
James Clarke <james@clarkezone.net>
2025-02-08 09:08:14 -0800

    Livescribe

---
+++ livestreams/LiveScribe/LiveScribe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveScribe
+//
+//  Created by James Clarke on 2/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ livestreams/LiveScribe/LiveScribe/LiveScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveScribeApp.swift
+//  LiveScribe
+//
+//  Created by James Clarke on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
