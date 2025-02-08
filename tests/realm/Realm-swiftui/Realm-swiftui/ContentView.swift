7b3d4b20b6f826e374a0e383841a3d1851b1b924
James Clarke <james@clarkezone.net>
2025-02-08 09:07:03 -0800

    ream

---
+++ tests/realm/Realm-swiftui/Realm-swiftui/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Realm-swiftui
+//
+//  Created by James Clarke on 1/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tests/realm/Realm-swiftui/Realm-swiftuiTests/Realm_swiftuiTests.swift
@@ -0,0 +1,16 @@
+//
+//  Realm_swiftuiTests.swift
+//  Realm-swiftuiTests
+//
+//  Created by James Clarke on 1/28/25.
+//
+
+import Testing
+
+struct Realm_swiftuiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
